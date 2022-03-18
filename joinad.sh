c024efdb95e90bcc82e2e254a245b60f5d3b48cf
tsanchez95 <buddysanchez86@gmail.com>
2022-03-18 11:31:42 -0700

    JoinAD updated

---
+++ joinad.sh
@@ -0,0 +1,76 @@
+#! /bin/bash
+sudo -i
+sudo yum -y update
+sudo hostnamectl set-hostname KPSLINUX02.vault.kps-lv.com
+sed -i 's/localhost /localhost KPSLINUX02 KPSLINUX02.vault.kps-lv.com /' /etc/hosts
+sudo yum -y install realmd sssd sssd-tools libnss-sss libpam-sss adcli samba-common-bin oddjob oddjob-mkhomedir packagekit krb5-user
+sudo cp /etc/hosts /etc/host_original
+sudo cat >> /etc/hosts <<EOL
+10.1.200.5    vault.kps-lv.com
+EOL
+sudo cp /etc/sudoers /etc/sudoers_original
+sudo cat >> /etc/sudoers <<EOL
+twotronz@vault.kps-lv.com ALL=(ALL:ALL) ALL
+ansible                   ALL=(ALL:ALL) ALL
+EOL
+head -n 2 /etc/hosts > /tmp/tmp.txt && mv /tmp/tmp.txt /etc/hosts -f
+sed -i 's/^.*PasswordAuthentication.*$/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sudo systemctl restart sshd.service
+sudo cat >> /etc/security/limits.conf <<EOL
+* hard core 0
+EOL
+
+sudo cat >> /etc/ntp.conf <<EOL
+restrict -4 default kod nomodify notrap nopeer noquery
+OPTIONS="-u ntp:ntp -p /var/run/ntpd.pid -g"
+EOL
+
+df --local -P | awk {'if (NR!=1) print $6'} | xargs -I '{}' find '{}' -xdev -type d -perm -0002 2>/dev/null | xargs chmod a+t
+
+sudo cat >> /etc/sysctl.conf <<EOL
+fs.suid_dumpable=0
+kernel.randomize_va_space = 2
+net.ipv4.ip_forward = 0
+net.ipv4.conf.all.send_redirects = 0
+net.ipv4.conf.default.send_redirects = 0
+net.ipv4.conf.all.accept_source_route = 0
+net.ipv4.conf.default.accept_source_route = 0
+net.ipv4.conf.all.accept_redirects = 0
+net.ipv4.conf.default.accept_redirects = 0
+net.ipv4.conf.all.secure_redirects = 0
+net.ipv4.conf.default.secure_redirects = 0
+net.ipv4.conf.all.log_martians = 1
+net.ipv4.conf.default.log_martians = 1
+net.ipv4.conf.default.rp_filter = 1
+net.ipv4.tcp_syncookies = 1
+net.ipv4.icmp_echo_ignore_broadcasts = 1
+net.ipv4.icmp_ignore_bogus_error_responses = 1
+net.ipv6.conf.all.accept_ra = 0
+net.ipv6.conf.default.accept_ra = 0
+net.ipv6.conf.all.accept_redirects = 0
+net.ipv6.conf.default.accept_redirects = 0
+EOL
+
+sudo chown root:root /etc/issue
+sudo chmod 644 /etc/issue
+
+grep ^gpgcheck /etc/yum.repos.d/*
+
+sudo cat >> /etc/rsyslog.conf <<EOL
+$FileCreateMode 0640
+*.*   @@10.238.42.36:514
+*.*   @@10.238.43.7:514
+EOL
+service rsyslog restart
+sudo update-motd --disable
+sudo cat >> /etc/motd <<EOL
+---------------------------------------------WARNING--------------------------------------------------
+
+This system is restricted to authorized users for legitimate business purposes and is subject to audit. 
+The unauthorized access, use, or modification of the computer systems, information resources, 
+and/or data contained therein or in transit to/from the system is illegal and will be prosecuted. 
+By using this system as an authorized user you agree to follow the Acceptable Use Policy. 
+You acknowledge receipt of the above message when you login.
+
+------------------------------------------------------------------------------------------------------------
+EOL
