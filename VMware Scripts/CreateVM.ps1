eb7b75f247166825ec5f94e545c2201dcaac3a69
Tomas Sanchez <buddysanchez86@gmail.com>
2022-03-22 16:56:50 -0700

    Updated powershell

--- VMware Scripts/CreateVM.ps1
+++ VMware Scripts/CreateVM.ps1
@@ -4,58 +4,23 @@
 # Type the VM Name
 $Name = Read-Host 'Type VM Name'
 
-# Select the Template
-$VMTemplate = "Ubunutu2004_011222"
+# Template
+#$VMTemplate = " "
 
-# Select the Host
-$VMHosts = Get-Content C:\Users\twotronz\Documents\VMHosts.csv
-$VMHost = $VMHosts | Out-GridView -Title "VM Hosts" -PassThru
+# Hosted Services
+$ResourcePool = "Hosted Services"
 
-# Select the Datastore
-$Datastores = Get-Content C:\Users\twotronz\Documents\Datastores.csv
-$Datastore = $Datastores | Out-GridView -Title "Datastores" -PassThru
+# Datastore
+$Datastore = "1 - HOST 1 - LOCAL"
 
-# Select the Network Name
-$NetworkNames = Get-Content C:\Users\twotronz\Documents\VNics.csv
-$NetworkName = $NetworkNames | Out-GridView -Title "VNICS" -PassThru
+# Network Name
+$NetworkName = "90 - Hosting"
 
-# Select Network Adapter Type
+# Network Adapter Type
 $NetworkAdapterType = "VMXNET 3"
 
-# Select the OS Spec
-$OSSpec = "MYBOFI-Ubuntu2004"
-
-# Type Annotations
-$Application = Read-Host 'Type Application Name'
-$ApplicationOwner = Read-Host 'Type Application Owner Name'
-$ApplicationRole = Read-Host 'Type Application Role Name'
-$ApplicationSupport = Read-Host 'Type Application Support Name'
-$BuiltBy = Read-Host 'Type Built By Name'
-$BuiltDate = Read-Host 'Type Built Date'
-$BusinessUnit = Read-Host 'Type Business Unit'
-$Department = Read-Host 'Type Department Name'
-$Environment = Read-Host 'Type Environment Name'
-$SupportOwner = Read-Host 'Type Support Owner Name'
-$SupportOwnerDL = Read-Host 'Type Support Owner DL Name'
-$SystemsSupport = Read-Host 'Type Systems Support Name'
-
-
 # Create VM Script
-New-VM -Name $Name -Template $VMTemplate –VMHost $VMHost -Datastore $Datastore -NetworkName $NetworkName -OSCustomizationSpec $OSSpec
+New-VM -Name $Name -ResourcePool $ResourcePool -Datastore $Datastore -NetworkName $NetworkName
 
 # Set Network Adapter
 Set-NetAdapter -Type $NetworkAdapterType -Connected:$true
-
-# Set VM Annotations
-Set-Annotation -Entity $Name -CustomAttribute "Application" -Value $Application
-Set-Annotation -Entity $Name -CustomAttribute "Application Owner" -Value $ApplicationOwner
-Set-Annotation -Entity $Name -CustomAttribute "Application Role" -Value $ApplicationRole
-Set-Annotation -Entity $Name -CustomAttribute "Application Support" -Value $ApplicationSupport
-Set-Annotation -Entity $Name -CustomAttribute "Built By" -Value $BuiltBy
-Set-Annotation -Entity $Name -CustomAttribute "Built Date" -Value $BuiltDate
-Set-Annotation -Entity $Name -CustomAttribute "BusinessUnit" -Value $BusinessUnit
-Set-Annotation -Entity $Name -CustomAttribute "Department" -Value $Department
-Set-Annotation -Entity $Name -CustomAttribute "Environment" -Value $Environment
-Set-Annotation -Entity $Name -CustomAttribute "Support Owner" -Value $SupportOwner
-Set-Annotation -Entity $Name -CustomAttribute "Support Owner DL" -Value $SupportOwnerDL
-Set-Annotation -Entity $Name -CustomAttribute "Systems Support" -Value $SystemsSupport
